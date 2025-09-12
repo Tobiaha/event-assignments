1a3853b7bebebfe816df12934099f3b2f43e6e69
Tobias <tobiashammarberg1@gmail.com>
2025-09-12 20:28:20 +0300

    Modalworkinprogress

--- t1/t1.js
+++ t1/t1.js
@@ -1,3 +1,4 @@
+'use strict';
 // array for todo list
 const todoList = [
   {
@@ -28,3 +29,35 @@ const todoList = [
 ];
 
 // add your code here
+const ul = document.getElementById('todoUI'); // ul target elementti
+const addButton = document.getElementById('addButton');
+const form = document.getElementById('todoForm');
+const dialog = document.getElementById('todoDialog');
+const input = document.getElementById('taskInput');
+
+function todoModal() {
+  ul.innerHTML = '';
+  todoList.forEach(item => {
+    const li = document.createElement('li');
+
+    checkbox.addEventListener('change', () => {
+      item.completed = checkbox.checked; // true false
+      console.log(`${item.task} -> ${item.comp}`);
+    });
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `todo-${item.id}`);
+    label.textContent = item.task;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `todo-${item.id}`;
+    checkbox.checked = item.completed;
+
+    label.prepend(checkbox);
+    li.appendChild(label);
+  });
+}
+todoModal();
+
+//Modal not working
